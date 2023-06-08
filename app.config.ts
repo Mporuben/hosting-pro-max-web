fb298ed90e411c50f89f0ca78380800896974bd2
matusporuben <matus.poruben@gmail.com>
2023-06-08 23:45:31 +0200

    init content

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,8 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Hosting Pro Max',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
     aside: {
       level: 0,
       collapsed: false,
@@ -24,18 +13,16 @@ export default defineAppConfig({
       fluid: true
     },
     header: {
-      logo: true,
+      logo: false,
       showLinkIcon: true,
       exclude: [],
       fluid: true
     },
     footer: {
-      iconLinks: [
-        {
-          href: 'https://nuxt.com',
-          icon: 'simple-icons:nuxtdotjs'
-        }
-      ]
+      credits: {
+        text: 'Build with ❤️ by Hosting Pro Max team',
+        icon: ''
+      },
     }
   }
 })
